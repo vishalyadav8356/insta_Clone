b30c000a99db8cc2636ba3f332943095f95ea967
vishal yadav <vishalyadav4052@gmail.com>
2026-03-03 01:30:29 +0530

    cors added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:5173"
+}))
 
 
 const authRouter = require("./routes/auth.routes")
