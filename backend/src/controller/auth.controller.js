6eb9e1c893550b797b3f643637c0a9372fd6870d
vishal yadav <vishalyadav4052@gmail.com>
2026-03-03 15:32:03 +0530

    updated code

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -94,7 +94,24 @@ async function loginController (req, res) {
   });
 }
 
+async function getMeCollection(req, res){
+
+  const userId = req.user.id
+  const user =  await userModel.findById(userId)
+
+res.status(200).json({
+  user:{
+    username: user.username,
+    email: user.email,
+    bio: user.bio,
+    profileImage: user.profileImage
+  } 
+})
+}
+
+
 module.exports = {
     registerController,
-    loginController
+    loginController,
+    getMeCollection
 }
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const authController = require("../controller/auth.controller")
+const identifyUser = require("../middlewares/auth.middleware")
 
 const authRouter = express.Router();
 
 authRouter.post("/register", authController.registerController);
 
 authRouter.post("/login", authController. loginController);
+
+authRouter.get("/get-me", identifyUser ,authController.getMeCollection)
 
 module.exports = authRouter;
